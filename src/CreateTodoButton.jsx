18ec45d586eff7335aabb44e653ee63bc7d80c83
Leonardo <49700921+LeonardoHOjeda@users.noreply.github.com>
2022-03-06 00:13:01 -0600

    Initial Commit
    Firts commit of TODO LIST React App

---
+++ src/CreateTodoButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './CreateTodoButton.css';
+
+function CreateTodoButton(){
+  const onClickButton = (msg) => {
+    alert(msg);
+  }
+  return(
+    <button className="CreateTodoButton" onClick={() => onClickButton('Hola')}>+</button>
+  );
+}
+
+export {CreateTodoButton};
---
+++ src/TodoSearch.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './TodoSearch.css';
+
+
+function TodoSearch({searchValue, setSearchValue}){
+
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  }
+
+  return(
+    <input className="TodoSearch" placeholder="Buscar un TODO..." onChange={onSearchValueChange} value={searchValue}/>
+  )
+}
+
+export {TodoSearch};
